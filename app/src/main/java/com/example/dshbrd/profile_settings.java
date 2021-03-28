ed3c43bbe74fd035d6a37c29e9fbdfaa2074af42
Loghin024 <loghincatalin024@gmail.com>
2021-03-28 20:31:27 +0300

    dashboard

---
+++ app/src/main/java/com/example/dshbrd/profile_settings.java
@@ -0,0 +1,14 @@
+package com.example.dshbrd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile_settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
